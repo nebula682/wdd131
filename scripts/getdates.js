de71d752d64853e7354e3bb1582b2c2e4a114bd0
nebula682 <malcolmnie@gmail.com>
2024-11-06 22:58:07 +0200

    FIRST

---
+++ scripts/getdates.js
@@ -0,0 +1,5 @@
+    //Populate the current year
+    document.getElementById('currentyear').textContent = new Date().getFullYear();
+                
+    //Populate last modified date
+   document.getElementById('lastModified').textContent = "Last modified: " + document.lastModified;
